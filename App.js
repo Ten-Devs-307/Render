8cece69be556c1d08366427a2b21e6790ecb3e06
Jonathan Kofi Donkor <jkd6735@gmail.com>
2022-03-10 19:04:23 +0000

    test screen

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+// import SignInScreen from './screens/SignInScreen/SignInScreen'
+// import SignUpScreen from './screens/SignUpScreen/SignUpScreen'
+import TestScreen from './src/screens/TestScreen/TestScreen'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <SafeAreaView style={styles.root}>
+
+    <TestScreen />
+    
+    <StatusBar style="auto" />
+  </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    // backgroundColor: '#fff',
+    // backgroundColor: '#F8EDED',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#F8EDED',
   },
 });
---
+++ src/components/CustomInput/CustomInput.js
@@ -0,0 +1,38 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+
+const CustomInput = ({ value, setValue, placeholder, secureTextEntry  }) => {
+	return (
+		<View style={styles.container}>
+			<TextInput 
+            value = {value}
+            onChangeText = {setValue}
+            placeholder= {placeholder} 
+            style={styles.input}
+            secureTextEntry={secureTextEntry}
+             />
+            
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		// backgroundColor: "white",
+		width: "100%",
+		borderColor: "#000",
+		// borderRadius: 5,
+		borderBottomWidth: 2,
+		// paddingHorizontal: 10,
+		paddingVertical: 5,
+		marginBottom: 30,
+		marginVertical: 10,
+	
+	},
+	input: {
+		fontSize: 20,
+		color: '#000',
+	},
+});
+
+export default CustomInput;
