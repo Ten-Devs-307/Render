e1c19c4e415658fd8805996b6233fcc8ede335c7
Jonathan Kofi Donkor <jkd6735@gmail.com>
2022-04-24 15:07:35 +0000

    update on forgot password

--- src/screens/ForgotPassword/ForgotPassword.js
+++ src/screens/ForgotPassword/ForgotPassword.js
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 
 const ForgotPassword = () => {
 	return (
-		<View>
+		<View style={styles.root}>
 			<Text>ForgotPassword</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	root: {
+		// alignItems: 'center',
+		justifyContent: "center",
+		flex: 1,
+		alignItems: "center",
+		// paddingHorizontal: 20,
+	},
+});
 
 export default ForgotPassword;
