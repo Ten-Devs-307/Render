9813bc794071165e1c402f617ad98fc11660ef32
Jonathan Kofi Donkor <jkd6735@gmail.com>
2022-05-04 20:07:20 +0000

    update

--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -1,60 +1,81 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, useWindowDimensions } from 'react-native'
-import React,{useContext, useState} from 'react'
-import Logo from '../../../assets/images/Render.png';
-import CustomButton from '../../components/CustomButton'
-import CustomInput from '../../components/CustomInput'
-import {useNavigation} from '@react-navigation/native';
-import { AuthContext } from '../../context/AuthContext';
-import Spinner from 'react-native-loading-spinner-overlay';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	Image,
+	useWindowDimensions,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import Logo from "../../../assets/images/Render.png";
+import CustomButton from "../../components/CustomButton";
+import CustomInput from "../../components/CustomInput";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../context/AuthContext";
+import Spinner from "react-native-loading-spinner-overlay";
+import { COLORS } from "../../constants/theme";
+import { Touchable } from "react-native-web";
 const SignUpScreen = () => {
-  const [email, setEmail] = useState(null);
-  const [password1, setPassword1] = useState(null);
-  const [password2, setPassword2] = useState(null);
+	const [email, setEmail] = useState(null);
+	const [password1, setPassword1] = useState(null);
+	const [password2, setPassword2] = useState(null);
 
+	const navigation = useNavigation();
 
 	const onLoginPress = () => {
 		navigation.navigate("SignIn");
 	};
 
-//   const {isLoading, register} = useContext(AuthContext);
-//   const val = useContext(AuthContext);
+	//   const {isLoading, register} = useContext(AuthContext);
+	//   const val = useContext(AuthContext);
 	const val = "This is a trial";
 
-  return (
-    <View style={styles.root}>
-      {/* <Spinner visible={isLoading}/> */}
-      <Image source={Logo} style={[styles.logo]}
-          resizeMode="contain" />
+	return (
+		<View style={styles.root}>
+			{/* <Spinner visible={isLoading}/> */}
+			<Image source={Logo} style={[styles.logo]} resizeMode="contain" />
 
+			<Text style={styles.header}>Create an account.</Text>
+			<Text>{val}</Text>
+			<Text style={styles.label}>Email</Text>
+			<CustomInput
+				placeholder=""
+				value={email}
+				onChangeText={(text) => setEmail(text)}
+			/>
 
-      <Text style={styles.header} >Create an account.</Text>
-      <Text>{val}</Text>
-      <Text style={styles.label} >Email</Text>
-      <CustomInput placeholder='' value={email} onChangeText={text => setEmail(text)} />
+			<Text style={styles.label}>Password</Text>
+			<CustomInput
+				placeholder=""
+				secureTextEntry={true}
+				value={password1}
+				onChangeText={(text) => setPassword1(text)}
+			/>
 
-      <Text style={styles.label} >Password</Text>
-      <CustomInput placeholder='' secureTextEntry={true} value={password1} onChangeText={text => setPassword1(text)}/>
+			<Text style={styles.label}>Re-enter Password</Text>
+			<CustomInput
+				placeholder=""
+				secureTextEntry={true}
+				value={password2}
+				onChangeText={(text) => setPassword2(text)}
+			/>
 
-      <Text style={styles.label} >Re-enter Password</Text>
-      <CustomInput placeholder='' secureTextEntry={true} value={password2} onChangeText={text => setPassword2(text)}/>
-
-
-      <CustomButton
-        text='Sign Up'
-        // onPress={() => {
-        // register(email, password1, password2);
-      // }}
-      />
-      <Text style={styles.text} >By signing up, you agree to our Terms and Service and Privacy Policy.</Text>
-         <Text style={styles.text} >Have an account?
-         <TouchableOpacity>
-           <Text style={styles.login}  onPress={onLoginPress} > Log In</Text>
-         </TouchableOpacity>
-         </Text>
-
-
-    </View>
-  );
+			<CustomButton
+				text="Sign Up"
+				// onPress={() => {
+				// register(email, password1, password2);
+				// }}
+			/>
+			<Text style={styles.text}>
+				By signing up, you agree to our Terms and Service and Privacy Policy.
+			</Text>
+			<TouchableOpacity>
+				<Text style={styles.text} onPress={onLoginPress}>
+					Have an account? Log In{" "}
+				</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
@@ -73,11 +94,11 @@ const styles = StyleSheet.create({
 		alignSelf: "center",
 		color: "#000",
 	},
-	login: {
-		fontWeight: "bold",
-		fontSize: 18,
-		marginLeft: 2,
-	},
+	// login: {
+	// 	fontWeight: "bold",
+	// 	color: COLORS.primary,
+	// 	fontSize: 18,
+	// },
 	logo: {
 		width: "80%",
 		maxWidth: 300,
