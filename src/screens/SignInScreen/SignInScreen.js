17934746ba0810fc040f5c6cca96270d2f3c9bce
Jonathan Kofi Donkor <jkd6735@gmail.com>
2022-05-08 14:04:52 +0000

    update

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -27,7 +27,7 @@ const SignInScreen = () => {
 	const navigation = useNavigation();
 
 	const onSignInPress = () => {
-		login(email, password);
+		login(email,password)
 		navigation.navigate("HomeScreen");
 	};
 
@@ -38,7 +38,7 @@ const SignInScreen = () => {
 	const [email, setEmail] = useState(null);
 	const [password, setPassword] = useState(null);
 
-	const { login } = useContext(AuthContext);
+	const {login} = useContext(AuthContext);
 	const val = "This is a trial";
 
 	return (
@@ -60,6 +60,7 @@ const SignInScreen = () => {
 				onChangeText={(text) => setPassword(text)}
 				value={password}
 			/>
+<<<<<<< HEAD
 			<CustomButton text="Sign In" onPress={onSignInPress} />
 
 			<Text style={styles.label}>Password</Text>
@@ -82,6 +83,12 @@ const SignInScreen = () => {
 					Don't have an account? <Text style={styles.signin}>Sign Up</Text>
 				</Text>
 			</TouchableOpacity>
+=======
+			<CustomButton text="Sign In" onPress={onSignInPress} /> 
+       <TouchableOpacity onPress={onSignUpPress} >
+         <Text style={styles.text} >Don't have an account? <Text style={styles.signin}>Sign Up</Text></Text>
+       </TouchableOpacity>
+>>>>>>> 71ab498ea4327c5ccae111dcc68a47f20c9bbb28
 		</View>
 	);
 };
