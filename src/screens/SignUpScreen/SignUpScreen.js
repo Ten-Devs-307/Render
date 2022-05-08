308be2087ac084bdb96a9f612b304f97fe46f14f
Jonathan Kofi Donkor <jkd6735@gmail.com>
2022-05-08 22:36:47 +0000

    update on sign up screen

--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -1,149 +1,151 @@
 import axios from "axios";
 import React, { useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  View,
-  Button,
-  Platform,
-  TextInput,
+	StyleSheet,
+	Text,
+	ScrollView,
+	View,
+	Button,
+	Platform,
+	TextInput,
 } from "react-native";
 import Constants from "expo-constants";
 import { useNavigation } from "@react-navigation/native";
-
+import Logo from "../../../assets/images/render.png";
 
 const baseUrl = "http://www.renderjobs.com/api";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  const onChangeNameHandler = (name) => {
-    setName(name);
-  };
+	const onChangeNameHandler = (name) => {
+		setName(name);
+	};
 
-  const onChangeEmailHandler = (email) => {
-    setEmail(email);
-  };
-  const onChangePasswordHandler = (password) => {
-    setPassword(password);
-  };
+	const onChangeEmailHandler = (email) => {
+		setEmail(email);
+	};
+	const onChangePasswordHandler = (password) => {
+		setPassword(password);
+	};
 
-  const onSubmitFormHandler = async (event) => {
-    if (!name.trim() || !email.trim() || !password.trim()) {
-      alert("Name or Email or password is invalid");
-      return;
-    }
-    setIsLoading(true);
-    try {
-      const response = await axios.post(`${baseUrl}/sign-up/`, {
-			email,
-			name,
-			password,
-		},
-		{headers: {'Content-Type': 'application/json'}}
-		 );
-      if (response.status === 200 || response.status === 201) {
-        alert('Your account has been created successfully');
-        setIsLoading(false);
-        setName('');
-        setEmail('');
-        setPassword('');
-        navigation.navigate("SignIn");
-      } else {
-        throw new Error(response.status);
-      }
-    } catch (error) {
-      alert('Registration failed');
-      setIsLoading(false);
-    }
-  };
+	const onSubmitFormHandler = async (event) => {
+		if (!name.trim() || !email.trim() || !password.trim()) {
+			alert("Name or Email or password is invalid");
+			return;
+		}
+		setIsLoading(true);
+		try {
+			const response = await axios.post(
+				`${baseUrl}/sign-up/`,
+				{
+					email,
+					name,
+					password,
+				},
+				{ headers: { "Content-Type": "application/json" } }
+			);
+			if (response.status === 200 || response.status === 201) {
+				alert("Your account has been created successfully");
+				setIsLoading(false);
+				setName("");
+				setEmail("");
+				setPassword("");
+				navigation.navigate("SignIn");
+			} else {
+				throw new Error(response.status);
+			}
+		} catch (error) {
+			alert("Registration failed");
+			setIsLoading(false);
+		}
+	};
 
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View>
-        <View style={styles.wrapper}>
-          {isLoading ? (
-            <Text style={styles.formHeading}> Creating resource </Text>
-          ) : (
-            <Text style={styles.formHeading}>Create new user</Text>
-          )}
-			  </View>
-			<View style={styles.wrapper}>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="#ffffff"
-            style={styles.input}
-            value={email}
-            editable={!isLoading}
-            onChangeText={onChangeEmailHandler}
-				  />
+	return (
+		<ScrollView contentContainerStyle={styles.container}>
+			<View>
+				<View style={styles.wrapper}>
+					{isLoading ? (
+						<Text style={styles.formHeading}> Creating resource </Text>
+					) : (
+						<Text style={styles.formHeading}>Create new user</Text>
+					)}
+				</View>
+				<View style={styles.wrapper}>
+					<TextInput
+						placeholder="Email"
+						placeholderTextColor="#ffffff"
+						style={styles.input}
+						value={email}
+						editable={!isLoading}
+						onChangeText={onChangeEmailHandler}
+					/>
+				</View>
+				<View style={styles.wrapper}>
+					<TextInput
+						placeholder="Full Name"
+						placeholderTextColor="#ffffff"
+						style={styles.input}
+						value={name}
+						editable={!isLoading}
+						onChangeText={onChangeNameHandler}
+					/>
+				</View>
+
+				<View style={styles.wrapper}>
+					<TextInput
+						placeholder="Password"
+						placeholderTextColor="#ffffff"
+						style={styles.input}
+						value={password}
+						editable={!isLoading}
+						secureTextEntry={true}
+						onChangeText={onChangePasswordHandler}
+					/>
+				</View>
+				<View>
+					<Button
+						title="Register"
+						onPress={onSubmitFormHandler}
+						style={styles.submitButton}
+						disabled={isLoading}
+					/>
+				</View>
 			</View>
-        <View style={styles.wrapper}>
-          <TextInput
-            placeholder="Full Name"
-            placeholderTextColor="#ffffff"
-            style={styles.input}
-            value={name}
-            editable={!isLoading}
-            onChangeText={onChangeNameHandler}
-          />
-        </View>
-
-        <View style={styles.wrapper}>
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#ffffff"
-            style={styles.input}
-            value={password}
-            editable={!isLoading}
-            secureTextEntry={true}
-            onChangeText={onChangePasswordHandler}
-          />
-        </View>
-        <View>
-          <Button
-            title="Register"
-            onPress={onSubmitFormHandler}
-            style={styles.submitButton}
-            disabled={isLoading}
-          />
-        </View>
-      </View>
-    </ScrollView>
-  );
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#252526",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
-  },
-  formHeading: {
-    color: "#ffffff",
-  },
-  wrapper: {
-    marginBottom: 10,
-  },
-  input: {
-    borderWidth: 2,
-    borderColor: "grey",
-    minWidth: 200,
-    textAlignVertical: "center",
-    paddingLeft: 10,
-    borderRadius: 20,
-    color: "#ffffff",
-  },
-  submitButton: {
-    backgroundColor: "gray",
-    padding: 100,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#252526",
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+	},
+	formHeading: {
+		color: "#ffffff",
+	},
+	wrapper: {
+		marginBottom: 10,
+	},
+	input: {
+		borderWidth: 2,
+		borderColor: "grey",
+		minWidth: 200,
+		textAlignVertical: "center",
+		paddingLeft: 10,
+		borderRadius: 20,
+		color: "#ffffff",
+	},
+	submitButton: {
+		backgroundColor: "gray",
+		padding: 100,
+	},
 });
